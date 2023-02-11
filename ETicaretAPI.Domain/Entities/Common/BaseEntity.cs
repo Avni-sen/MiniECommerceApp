12e69f6c4063d487426acd5eadb8481b7933f210
RSavnikaan <avni.kaan@realsolutions.com.tr>
2023-02-11 22:26:14 +0300

    Add project files.

---
+++ ETicaretAPI.Domain/Entities/Common/BaseEntity.cs
@@ -0,0 +1,11 @@
+﻿namespace ETicaretAPI.Domain.Entities.Common
+{
+    public class BaseEntity
+    {
+        public Guid Id { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public bool Status { get; set; }
+        public bool IsDeleted { get; set; }
+
+    }
+}
---
+++ ETicaretAPI.Persistance/ServiceRegistration.cs
@@ -0,0 +1,14 @@
+﻿using ETicaretAPI.Application.Abstractions;
+using ETicaretAPI.Persistance.Concrete;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace ETicaretAPI.Persistance
+{
+    public static class ServiceRegistration
+    {
+        public static void AddPersistenceServices(this IServiceCollection services)
+        {
+            services.AddSingleton<IProductService ,ProductService>();   
+        }
+    }
+}
